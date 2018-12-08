0272153dcd41db2f79156d411290874aeb813cd6
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-08 17:38:22 +0800

    Particles completed. But somehow not working.

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -7,7 +7,7 @@ const ImageLinkForm = () => {
             <p className="f3">
                 This Magic Brain will detect faces in your picture.
             </p>
-            <div>
+            <div className='center'>
                 <div className="form center pa4 br3 shadow-5">
                     <input type="text" className="f4 pa2 w-70 center"/>
                     <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple">Detect</button>
