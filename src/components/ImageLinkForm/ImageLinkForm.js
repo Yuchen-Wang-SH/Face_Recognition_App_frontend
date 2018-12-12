a694e9a1489e36cb62662469a40ee35f89e97436
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-12 21:13:51 +0800

    placeholder & center the logo

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -11,7 +11,7 @@ class ImageLinkForm extends Component {
                 </p>
                 <div className='center'>
                     <div className="form center pa4 br3 shadow-5">
-                        <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/>
+                        <input type="text" className="f4 pa2 w-70 center" placeholder='Please input the link of the picture here.' onChange={onInputChange}/>
                         <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onImageSubmit}>Detect</button>
                     </div>
                 </div>
