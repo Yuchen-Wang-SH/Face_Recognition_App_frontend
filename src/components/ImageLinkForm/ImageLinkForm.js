3f395082052ff8b6bef4b41ac00918a44e4bb5ff
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-12 17:13:39 +0800

    ImageLinkForm hit enter to submit

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,21 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './ImageLinkForm.css';
 
-const ImageLinkForm = ({ onInputChange, onImageSubmit }) => {
-    return (
-        <div>
-            <p className="f3">
-                This Magic Brain will detect faces in your picture.
-            </p>
-            <div className='center'>
-                <div className="form center pa4 br3 shadow-5">
-                    <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/>
-                    <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onImageSubmit}>Detect</button>
+class ImageLinkForm extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const { onInputChange, onImageSubmit } = this.props;
+        return (
+            <div>
+                <p className="f3">
+                    This Magic Brain will detect faces in your picture.
+                </p>
+                <div className='center'>
+                    <div className="form center pa4 br3 shadow-5">
+                        <input type="text" className="f4 pa2 w-70 center" onChange={onInputChange}/>
+                        <button className="w-30 grow f4 link ph3 pv2 dib white bg-light-purple" onClick={onImageSubmit}>Detect</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
 
-}
+    componentDidMount() {
+        const inputs = document.querySelectorAll('input');
+        inputs.forEach(input => {
+            input.addEventListener("keyup", function(event) {
+                // Cancel the default action, if needed
+                event.preventDefault();
+                // Number 13 is the "Enter" key on the keyboard
+                if (event.keyCode === 13) {
+                  // Trigger the button element with a click
+                  document.getElementsByTagName('button')[0].click();
+                }
+            })
+        })
+    }
+} 
 
 export default ImageLinkForm;
