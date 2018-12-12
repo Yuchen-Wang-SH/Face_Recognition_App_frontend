183762e05355516e6d3d14c437564965abf0dc7b
Yuchen Wang <yuchenwang.sh@gmail.com>
2018-12-12 10:55:35 +0800

    if no imageUrl, then display nothing, even a blank img tag

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -2,6 +2,10 @@ import React from 'react';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, boxes }) => {
+    if (!imageUrl) {
+        return null;
+    }
+
     const displayBoxesDiv = [];
     for (const box of boxes) {
         displayBoxesDiv.push(<div key='0' className="bounding-box" style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>)
